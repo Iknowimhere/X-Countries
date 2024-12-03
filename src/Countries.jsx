626caaa8dfeeee80304afad27e909445cf1a21b1
umashankar <umashankarp33@gmail.com>
2024-12-03 22:49:51 +0530

    add country card and fetch the data from backend to generate row of cards to display country data

---
+++ src/Countries.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+import { Country } from './Country';
+
+export const Countries = () => {
+  let [countries, setCountries] = useState([]);
+  let api = 'https://xcountries-backend.azurewebsites.net/all';
+  useEffect(() => {
+    fetch(api)
+      .then((res) => res.json())
+      .then((data) => setCountries(data))
+      .catch((err) => console.error(err));
+  }, []);
+    
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        gap: '1em',
+              alignItems: 'center',
+        textAlign: 'center',
+      }}
+    >
+      {countries.map((country) => (
+        <Country name={country.name} flag={country.flag} abbr={country.abbr} key={country.abbr}/>
+      ))}
+    </div>
+  );
+};
---
+++ src/Country.jsx
@@ -0,0 +1,21 @@
+export const Country = ({flag, name, abbr}) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        padding: '1em',
+              gap: '1em',
+              border: '1px solid grey',
+              borderRadius: '1em',
+              height: '200px',
+        width: '200px',
+      }}
+    >
+      <img src={flag} alt={abbr} height={100} width={100} />
+          <h2>{name}</h2>
+    </div>
+  );
+};
